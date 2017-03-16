d7fc10398fe0bf118eabd5961a97fd40489cf861
René Krell <renda.krell@gmail.com>
2017-03-16 10:21:52 +0100

    Applied 0011-more-rac-removal.patch

--- src/sis_hwmc.c
+++ src/sis_hwmc.c
@@ -43,7 +43,9 @@ THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "xf86.h"
 #include "xf86_OSproc.h"
+#ifndef GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 6
 #include "xf86Resources.h"
+#endif
 #include "compiler.h"
 #include "xf86PciInfo.h"
 #include "xf86Pci.h"
